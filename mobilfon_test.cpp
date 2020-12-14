b9035e2e9c0f4efa550275747d115a749200c523
odell744 <odell744@gmail.com>
2020-12-14 16:04:54 +0700

    Добавьте файлы проекта.

---
+++ mobilfon_test.cpp
@@ -0,0 +1,12 @@
+﻿// mobilfon_test.cpp: определяет точку входа для приложения.
+//
+
+#include "mobilfon_test.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
